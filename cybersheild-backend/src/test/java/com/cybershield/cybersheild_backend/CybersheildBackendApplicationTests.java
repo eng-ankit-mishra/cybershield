1613abd9dc77fb5811f7a758b63f6b5b4ab020f0
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-05-19 15:58:06 +0530

    Initial Setup

---
+++ cybersheild-backend/src/test/java/com/cybershield/cybersheild_backend/CybersheildBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybershield.cybersheild_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybersheildBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
